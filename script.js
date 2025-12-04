57cb1793621186eb853488878d94dc9464c3ff5c
Julien Baouz <baouzjulien@gmail.com>
2025-12-04 09:41:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function permuteString(string, prefix, array) {
+  prefix = "";
+  array = [];
   if(!string.length) {
     array.push(prefix);
     return array;
+  } else {
+    array =  Array.from(string).forEach((char) => {
+      array.splice(char, 1);
+      const stringIn = array.join("");
+      permuteString(stringIn ,char, array)
+    })
   }
+  return [...new Set(array)];
 }
